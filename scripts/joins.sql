5741ef2b09f3784943e97371715ce007005b72c4
Jerod Spence <spencejerod@gmail.com>
2023-02-11 13:57:24 -0600

    Last push for today

--- scripts/joins.sql
+++ scripts/joins.sql
@@ -58,4 +58,20 @@ WHERE headquarters NOT LIKE '%CA%'
 ORDER BY imdb_rating DESC;
 --- Two comapanies are not in CA. Dirty Dancing was the highest grossing film made a company not in CA(The comapny is Vestron Pictures in Chicago,Ill.)
 
--- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
+-- -- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
+-- SELECT  AVG(imdb_rating), COUNT(specs.length_in_min), COUNT(film_title)
+-- FROM specs 
+-- LEFT JOIN rating
+-- ON specs.movie_id=rating.movie_id
+-- WHERE length_in_min >'120'
+-- GROUP BY length_in_min, film_title,imdb_rating;
+-- GROUP BY specs.length_in_min, film_title, imdb_rating
+-- UNION ALL
+SELECT AVG(imdb_rating),AVG(COUNT(length_in_min)), AVG(COUNT(film_title))
+FROM specs 
+LEFT JOIN rating
+ON specs.movie_id=rating.movie_id
+WHERE length_in_min <'120'
+GROUP BY specs.length_in_min, film_title, imdb_rating
+ORDER BY  length_in_min DESC;
+---Movies over two hours long have a higher rating on average than movies less than two hours long.
